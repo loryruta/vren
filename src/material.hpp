bb85c5c0a24efd0bb3054bce01a8d3c1138e8fe5
Lorenzo Rutayisire <lorenzorutayisire@gmail.com>
2021-12-13 13:47:01 +0100

    Add material & material_manager

--- src/material.hpp
+++ src/material.hpp
@@ -1,61 +1,51 @@
 #pragma once
 
-#include <queue>
+#include <memory>
 
 #include <glm/glm.hpp>
 
 #include "vk_utils.hpp"
 #include "gpu_allocator.hpp"
-
-#define VREN_MATERIAL_TEXTURES_BINDING 1
-#define VREN_MATERIAL_COLORS_BINDING   2
-
-
-#define VREN_DESCRIPTOR_POOL_SIZE 32
+#include "light.hpp"
 
 namespace vren
 {
 	// Forward decl
 	class renderer;
 
-	struct material_uniform_buffer
-	{
-		glm::vec3 m_diffuse_color;
-		glm::vec3 m_specular_color;
-	};
-
 	struct material
 	{
-		glm::vec3 m_diffuse_color;
-		glm::vec3 m_specular_color;
+		uint32_t m_idx;
 
-		vren::texture m_diffuse_texture;
-		vren::texture m_specular_texture;
+		vren::texture m_albedo_texture;
 
-		material(vren::renderer& renderer);
+		float m_metallic;
+		float m_roughness;
+
+		explicit material(vren::renderer& renderer);
 	};
 
-	class material_descriptor_set_pool
+	class material_manager
 	{
 	private:
-		VkDescriptorSetLayout _create_descriptor_set_layout() const;
-		void _add_descriptor_pool();
-
-	public:
-		VkDescriptorSetLayout m_descriptor_set_layout;
-
-		std::vector<VkDescriptorPool> m_descriptor_pools;
-		size_t m_last_pool_allocated_count = 0;
-
-		std::queue<VkDescriptorSet> m_descriptor_sets;
+		std::vector<std::unique_ptr<material>> m_materials;
 
 		vren::renderer& m_renderer;
 
-		material_descriptor_set_pool(vren::renderer& renderer);
-		~material_descriptor_set_pool();
+		size_t m_material_struct_aligned_size;
 
-		void acquire_descriptor_sets(size_t descriptor_sets_count, VkDescriptorSet* descriptor_sets);
-		void update_descriptor_set(vren::material const* material, VkDescriptorSet descriptor_set);
-		void release_descriptor_set(VkDescriptorSet descriptor_set);
+		vren::gpu_buffer m_uniform_buffer;
+		size_t m_uniform_buffer_size;
+
+	public:
+		explicit material_manager(vren::renderer& renderer);
+		~material_manager();
+
+		material* create_material();
+		material* get_material(uint32_t idx);
+
+		void upload_device_buffer();
+
+		void update_material_descriptor_set(vren::material const* material, VkDescriptorSet descriptor_set);
 	};
 }
